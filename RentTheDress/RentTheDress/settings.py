98ff9a1c4754ef81577d5446d5e44d2aaa44dd31
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-03-09 11:07:22 -0600

    added bootstrap

--- RentTheDress/RentTheDress/settings.py
+++ RentTheDress/RentTheDress/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'Rent.apps.RentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- RentTheDress/RentTheDress/urls.py
+++ RentTheDress/RentTheDress/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^Rent/', include('Rent.urls')),
+    url(r'^rent/', include('Rent.urls')),
     url(r'^admin/', admin.site.urls),
 ]
