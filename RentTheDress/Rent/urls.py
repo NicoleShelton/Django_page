a1882b7ec2df751f4ea589763449e4f3e7d97445
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-03-09 11:34:48 -0600

    took out ^ for home page

--- RentTheDress/Rent/urls.py
+++ RentTheDress/Rent/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'RentTheDress'
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'$', views.home, name='home'),
     url(r'^inventory$', views.inventory, name='inventory'),
     url(r'^about$', views.about, name='about'),
 
