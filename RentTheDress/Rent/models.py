832dbdda30a038c47f3151bb1377ea92ba51dbcc
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-03-21 16:26:18 -0500

    wrote a model

--- RentTheDress/Rent/models.py
+++ RentTheDress/Rent/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Dress(models.Model):
+    dress = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+    image = models.URLField()
+    price = models.IntegerField()
+    description = models.CharField(max_length=50)
