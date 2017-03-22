ae9a0468006eaa4b7cd07bb8f91eaa51fcaa04cd
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-03-22 09:40:54 -0500

    cleared comments

--- RentTheDress/Rent/models.py
+++ RentTheDress/Rent/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 
-<<<<<<< HEAD
-# Create your models here.
-=======
->>>>>>> 63c748ed145b3ce8a6432fffe3db8708cbcf0f05
 class Dress(models.Model):
     dress = models.CharField(max_length=20)
     quantity = models.IntegerField()
     image = models.URLField()
     price = models.IntegerField()
-<<<<<<< HEAD
     description = models.CharField(max_length=50)
-=======
-    description = models.CharField(max_length=50)
->>>>>>> 63c748ed145b3ce8a6432fffe3db8708cbcf0f05
