74fa17c4bff1250d1c37e78341f46916eca2e1b6
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-03-23 13:24:39 -0500

    created a superuser for admin page

--- RentTheDress/Rent/models.py
+++ RentTheDress/Rent/models.py
@@ -3,6 +3,5 @@ from django.db import models
 class Dress(models.Model):
     name = models.CharField(max_length=20)
     quantity = models.IntegerField()
-    image = models.URLField()
     price = models.IntegerField()
     description = models.CharField(max_length=100)
