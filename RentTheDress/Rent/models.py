923f3771825e9dea0035c46b6dc56d6ce10ec5b8
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-03-23 09:11:11 -0500

    changed 'dress' to 'name' in the model

--- RentTheDress/Rent/models.py
+++ RentTheDress/Rent/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Dress(models.Model):
-    dress = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
     quantity = models.IntegerField()
     image = models.URLField()
     price = models.IntegerField()
