29840e450b664a717797e2b5231675c866f777b6
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-04-03 09:12:22 -0500

    removed the 'rent/' from urls

--- RentTheDress/RentTheDress/urls.py
+++ RentTheDress/RentTheDress/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^rent/', include('Rent.urls')),
+    url(r'^', include('Rent.urls')),
     url(r'^admin/', admin.site.urls),
 ]
